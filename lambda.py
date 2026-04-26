67d00c7513e2aaf73b031a5aee2c1aab19b86075
JEFIRO <jefiroo@gmail.com>
2026-04-26 13:36:52 -0300

    safgds

---
+++ lambda.py
@@ -0,0 +1,10 @@
+def lambda_handler(event, context):
+    print("hello world")
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": "{\"message\": \"It's working!\"}"
+    }
